5faa6e64e108bb6dbf19e187f54c3f484a1ac8f4
takrim1999 <takrimulalom@gmail.com>
2022-09-27 17:23:19 +0600

    Transpose Matrix function completed

--- takrim/matrix.py
+++ takrim/matrix.py
@@ -1,18 +1,38 @@
 #!/usr/bin/python3
+
+
 def trans(mat):
+    new_mat = [[None for k in range(len(mat))] for l in range(len(mat))]
+    for i in range(len(mat)):
+        for j in range(len(mat)):
+            # print(i,j,end='')
+            # print(' to ',end='')
+            # print(j,i,end='')
+            # print('\n')
+            new_mat[j][i] = mat[i][j]
+    return new_mat
+
+def det(mat):
+    pass
     
 
 order = int(input("order of your square matrix: "))
 matrix = [[None for k in range(order)] for j in range(order)]
-print(len(matrix))
+# print(len(matrix))
 # matrix[0][0].append('hello')
-print(matrix)
-# for i in range(order):
-#     print('row ' + str(i+1) + " data:")
-#     for j in range(order):
-#         matrix[i][j] = (int(input('')))
+# print(matrix)
+for i in range(order):
+    print('row ' + str(i+1) + " data:")
+    for j in range(order):
+        matrix[i][j] = (int(input('')))
 
-# print("your matrix is : ")
+print("your matrix is : ")
 
-# for col in matrix:
-#     print(col)
+for col in matrix:
+    print(col)
+# print(matrix)
+print("your transpose matrix is : ")
+
+for col in trans(matrix):
+    print(col)
+# print(trans(matrix))
