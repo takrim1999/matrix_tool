4bad88a17fd84cb06b7899dfdf8826005fa344a3
takrim1999 <takrimulalom@gmail.com>
2022-09-27 06:34:22 +0600

    started from takrim

---
+++ takrim/matrix.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def trans(mat):
+    
+
+order = int(input("order of your square matrix: "))
+matrix = [[None for k in range(order)] for j in range(order)]
+print(len(matrix))
+# matrix[0][0].append('hello')
+print(matrix)
+# for i in range(order):
+#     print('row ' + str(i+1) + " data:")
+#     for j in range(order):
+#         matrix[i][j] = (int(input('')))
+
+# print("your matrix is : ")
+
+# for col in matrix:
+#     print(col)
