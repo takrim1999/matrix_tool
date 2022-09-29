7b5fc706e70aa77ae26ed3a25c2b05f8486bf028
takrim1999 <takrimulalom@gmail.com>
2022-09-30 05:48:32 +0600

    added determinant option (currently work for 3x3 matrx only)

--- takrim/matrix.py
+++ takrim/matrix.py
@@ -5,22 +5,31 @@ def trans(mat):
     new_mat = [[None for k in range(len(mat))] for l in range(len(mat))]
     for i in range(len(mat)):
         for j in range(len(mat)):
-            # print(i,j,end='')
-            # print(' to ',end='')
-            # print(j,i,end='')
-            # print('\n')
             new_mat[j][i] = mat[i][j]
     return new_mat
 
+def process(mat):
+    new_matrices = {}
+    new_matrix = trans(mat)
+    for i in range(len(new_matrix)-1):
+        new_matrix.append(new_matrix[i])
+    # print(new_matrix)
+    for i in range(len(mat)):
+        new_matrices[new_matrix[i][0]] = new_matrix[i+1][1:],new_matrix[i+2][1:]
+    return new_matrices.items()    
+
+def calc(data):
+    return data[0]*((data[1][0][0]*data[1][1][1])-(data[1][1][0]*data[1][0][1]))
+    # print(data[1][0][0])
 def det(mat):
-    pass
-    
+    sum = 0
+    for i in process(mat):
+            sum = sum + calc(i)
+    return sum
+
 
 order = int(input("order of your square matrix: "))
 matrix = [[None for k in range(order)] for j in range(order)]
-# print(len(matrix))
-# matrix[0][0].append('hello')
-# print(matrix)
 for i in range(order):
     print('row ' + str(i+1) + " data:")
     for j in range(order):
@@ -35,4 +44,4 @@ print("your transpose matrix is : ")
 
 for col in trans(matrix):
     print(col)
-# print(trans(matrix))
+print("determinent is : " + str(det(matrix)))
